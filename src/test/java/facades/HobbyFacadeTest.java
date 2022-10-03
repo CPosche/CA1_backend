361d06e69cf5ffd855446efe23818f46b5c4f63b
Kristian Hartmann <hartkrist@gmail.comm>
2022-10-03 11:46:53 +0200

    implemented SuperFacadeTest to setup for all test classes

--- src/test/java/facades/HobbyFacadeTest.java
+++ src/test/java/facades/HobbyFacadeTest.java
@@ -13,7 +13,7 @@ import javax.persistence.EntityManagerFactory;
 
 import static junit.framework.Assert.assertEquals;
 
-public class HobbyFacadeTest {
+public class HobbyFacadeTest extends SuperFacadeTest{
 
     private static EntityManagerFactory emf;
     private static HobbyFacade facade;
@@ -29,39 +29,7 @@ public class HobbyFacadeTest {
 
     @BeforeEach
     public void setUp() {
-        EntityManager em = emf.createEntityManager();
-        try {
-            em.getTransaction().begin();
-            em.createNamedQuery("Phone.deleteAllRows").executeUpdate();
-            em.createNativeQuery("ALTER TABLE phone AUTO_INCREMENT = 1").executeUpdate();
-            em.createNativeQuery("DELETE FROM hobby_has_person").executeUpdate();
-            em.createNamedQuery("Hobby.deleteAllRows").executeUpdate();
-            em.createNativeQuery("ALTER TABLE hobby AUTO_INCREMENT = 1").executeUpdate();
-            em.createNamedQuery("Person.deleteAllRows").executeUpdate();
-            em.createNativeQuery("ALTER TABLE person AUTO_INCREMENT = 1").executeUpdate();
-            em.createNamedQuery("Address.deleteAllRows").executeUpdate();
-            em.createNativeQuery("ALTER TABLE address AUTO_INCREMENT = 1").executeUpdate();
-            em.createNamedQuery("Cityinfo.deleteAllRows").executeUpdate();
-            em.createNativeQuery("ALTER TABLE cityinfo AUTO_INCREMENT = 1").executeUpdate();
-
-            Phone phone = new Phone(71241337, "Ny telefon");
-            Person testPerson = new Person("Test", "Person", "Test@Person.dk");
-            testPerson.addPhones(phone);
-            Hobby badminton = new Hobby("Badminton", "Vi spiller badminton hver torsdag kl 16");
-            Address address = new Address("Solvej 2");
-            Cityinfo cityinfo = new Cityinfo(3450, "Alleroed");
-            cityinfo.addAddress(address);
-            testPerson.addAddress(address);
-            testPerson.addHobby(badminton);
-            Person testPerson2 = new Person("Test 2", "Person 2", "Test2@Person2.dk");
-            testPerson2.addHobby(badminton);
-            testPerson2.addAddress(address);
-            em.persist(testPerson);
-            em.persist(testPerson2);
-            em.getTransaction().commit();
-        } finally {
-            em.close();
-        }
+        super.setUp(emf);
     }
 
     

35dd7adc4910a0046617a28b5dc560c799a6ab94
Kristian Hartmann <hartkrist@gmail.comm>
2022-10-04 11:19:45 +0200

    Implented  getallhobbies

--- src/main/java/facades/HobbyFacade.java
+++ src/main/java/facades/HobbyFacade.java
@@ -1,12 +1,17 @@
 package facades;
 
 import dtos.HobbyDto;
+import dtos.PersonDto;
 import dtos.PhoneDto;
 import entities.Hobby;
+import entities.Person;
 import entities.Phone;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import java.util.LinkedHashSet;
+import java.util.List;
 
 public class HobbyFacade {
 
@@ -35,5 +40,12 @@ public class HobbyFacade {
     }
 
 
-
+    public List<HobbyDto> getAllHobby(){
+        EntityManager em = getEntityManager();
+        TypedQuery<Hobby> query = em.createQuery("SELECT p FROM Hobby p", Hobby.class);
+        List<Hobby> hobbies = query.getResultList();
+        if (hobbies == null)
+            return null;
+        return HobbyDto.getDtos(new LinkedHashSet<>(hobbies));
+    }
 }

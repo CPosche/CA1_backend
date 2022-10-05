ee66569399bbd43372574a464022ee03399568d3
Casper Posche <cbp@posche.dk>
2022-10-05 08:38:45 +0200

    delete address(the easy way), hopefully

--- src/main/java/facades/AddressFacade.java
+++ src/main/java/facades/AddressFacade.java
@@ -7,6 +7,8 @@ import entities.Person;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class AddressFacade {
 
@@ -34,6 +36,22 @@ public class AddressFacade {
         return emf.createEntityManager();
     }
 
+    public AddressDto deleteAddress(int id){
+        EntityManager em = getEntityManager();
+        Address address = em.find(Address.class, id);
+        TypedQuery<Person> query = em.createQuery("select p from Person p join p.fkAddress a where a.id = :id", Person.class);
+        List<Person> persons = query.getResultList();
+        em.getTransaction().begin();
+        for (Person person : persons) {
+            person.setFkAddress(null);
+            em.persist(person);
+        }
+        em.getTransaction().commit();
+        em.getTransaction().begin();
+        em.remove(address);
+        em.getTransaction().commit();
+        return new AddressDto(address);
+    }
 
 
 }
